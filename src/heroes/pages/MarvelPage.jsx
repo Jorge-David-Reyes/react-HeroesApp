0a0e2fd3eb0e31db4af5ab457c343897e506250b
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-03-09 13:41:59 -0600

    Lista de Heroes

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,5 +1,11 @@
+import { HeroList } from "../components";
+
 export const MarvelPage = () => {
   return (
-    <h1>MarvelPage</h1>
+    <>
+        <h1>MarvelPage</h1>
+        <hr />
+        <HeroList publisher="Marvel Comics"/>
+    </>
   )
 }
