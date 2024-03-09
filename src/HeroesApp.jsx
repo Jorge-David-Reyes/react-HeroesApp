77c36d1874e4401d08d1011ab79abd00a254e21c
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-03-09 08:34:44 -0600

    Inicio de proyecto - HeroesApp

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,5 @@
+export const HeroesApp = () => {
+  return (
+    <div>HeroesApp</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css'
+import { HeroesApp } from './HeroesApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>,
+)
