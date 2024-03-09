ee3499da678bc0d982e1ad9e5d6fc3396db30535
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-03-09 14:06:31 -0600

    Tarjetas con la informacion del Heroe

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const HeroCard = ({
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+}) => {
+
+    const heroImageUrl = `/assets/heroes/${ id }.jpg`;
+
+  return (
+
+    <div className="col">
+        <div className="card">
+            <div className="row no-gutters">
+                <div classs="col-4">
+                    <img src={heroImageUrl} className="card-img" alt={superhero} />
+                </div>
+
+                <div className='col-8'>
+                    <div className="card-body">
+                        <h5 className="card-title">{superhero}</h5>
+                        <p className="card-text">{ alter_ego }</p>
+
+                        <p>{characters}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+
+import { HeroCard } from './';
 import { getHeroesByPublisher } from '../helpers';
 
 export const HeroList = ({ publisher }) => {
@@ -6,12 +7,15 @@ export const HeroList = ({ publisher }) => {
     const heroes = getHeroesByPublisher( publisher );
 
   return (
-    <ul>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
         {
             heroes.map( (hero) => (
-                <li key={ hero.id }>{ hero.superhero }</li>
+                <HeroCard 
+                    key={hero.id}
+                    {...hero}
+                />
             ))
         }
-    </ul>
+    </div>
   )
 }
