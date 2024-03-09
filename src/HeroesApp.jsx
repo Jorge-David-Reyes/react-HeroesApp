654a51a21882a10ccd0f2d000a8ffe2a13feedf2
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-03-09 08:56:47 -0600

    Creando un primer Router

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,5 +1,9 @@
+import { AppRouter } from './router/AppRouter'
+
 export const HeroesApp = () => {
   return (
-    <div>HeroesApp</div>
+    <>
+      <AppRouter></AppRouter>
+    </>
   )
 }
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { DcPage } from "../heroes/pages/DcPage"
+import { LoginPage } from "../auth/pages/LoginPage"
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+            <Route path="marvel" element={<MarvelPage />} />
+            <Route path="dc" element={<DcPage />} /> 
+
+            <Route path="login" element={<LoginPage />} />
+
+            <Route path="/" element={<Navigate to="/marvel"/>} />   
+        </Routes>  
+    </>
+  )
+}
