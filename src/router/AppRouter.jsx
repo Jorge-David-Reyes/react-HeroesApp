7875ff378feeb55c9573597fce755c073f9245dc
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-03-09 09:44:31 -0600

    Colocar clase de la ruta activa

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { LoginPage } from "../auth/pages/LoginPage"
+
+import { Navbar } from "../ui";
+import { LoginPage } from '../auth';
+import { MarvelPage, DcPage } from '../heroes'; 
 
 export const AppRouter = () => {
   return (
     <>
+        <Navbar>
+
+        </Navbar>
         <Routes>
             <Route path="marvel" element={<MarvelPage />} />
             <Route path="dc" element={<DcPage />} /> 
