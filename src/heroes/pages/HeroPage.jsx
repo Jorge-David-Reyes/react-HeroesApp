f1dd8cbea44bd687eaac468eecb3fc5902a780b1
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-03-14 15:41:44 -0600

    200 - Nota: useMemo

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,3 +1,4 @@
+import React, { useMemo } from "react"
 import { Navigate, useParams, useNavigate } from "react-router-dom"
 import { getHeroById } from "../helpers"
 
@@ -6,7 +7,7 @@ export const HeroPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const hero = getHeroById(id);
+  const hero = useMemo(() => getHeroById(id), [id]);
 
   const onNavigateBack = () => {
     navigate(-1);
