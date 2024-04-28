ada876dcd649ba2bd6ac3a23397811910d16895f
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-04-27 21:59:52 -0600

    214 - Mantener el usuario activo

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,20 +4,31 @@ import { authReducer } from './authReducer'
 import { types } from '../types/types';
 
 // Esto es el estado inicial de la aplicación
-const initialState = {
-    logged: false,
+// const initialState = {
+//     logged: false,
+// }
+
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user, // '!!' convierte un valor a booleano, si user es null, logged será false, si user no es null, logged será true
+    user: user,
+  }
 }
 
 // Esta función es un componente que envuelve a todos los componentes que estén dentro de él
 export const AuthProvider = ({ children }) => {
 
   // useReducer es un hook que permite manejar el estado de la aplicación
-  const [authState, dispatch] = useReducer(authReducer, initialState); // authState es el estado de la aplicación y dispatch es una función que permite enviar acciones al reducer
+  const [authState, dispatch] = useReducer(authReducer, {}, init); // authState es el estado de la aplicación y dispatch es una función que permite enviar acciones al reducer
 
   const login = ( name = '' ) => {
 
     const user = { id: 'ABC', name }
     const action = { type: types.login, payload: user }
+
+    localStorage.setItem('user', JSON.stringify(user));
 
     dispatch(action);
   }
