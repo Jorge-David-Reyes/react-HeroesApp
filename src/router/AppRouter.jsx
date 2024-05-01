7938b6d53901525e6b13b5bbdf82c7a3647237a1
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-04-30 20:56:49 -0600

    215 - Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes'; 
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
   return (
@@ -10,7 +11,13 @@ export const AppRouter = () => {
         <Routes>
             <Route path="login" element={<LoginPage />} />
 
-            <Route path="/*" element={<HeroesRoutes />} />
+            <Route path="/*" element={
+              <PrivateRoute>
+                <HeroesRoutes />
+              </PrivateRoute>
+            } />
+
+            {/* <Route path="/*" element={<HeroesRoutes />} /> */}
         </Routes>  
     </>
   )
