ce83f1de89c34eaeca22b7680cd6ff64a114c725
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-05-01 15:39:30 -0600

    229 - Solucion de la tarea

---
+++ tests/ui/components/Navbar.test.jsx
@@ -0,0 +1,9 @@
+describe('Pruebass en el componente del <NavBar/>', () => {
+    test('debe de mostrar el nombre del usuario', () =>{
+
+    });
+
+    test('debe de llamar el logout y navogate cuando se hace click en el boton de logout', () =>{
+    
+    });
+})
